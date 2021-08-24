6fc469edab1daef9b2287887e036bef15aa45edc
kama casta <kamacasta983@gmail.com>
2021-08-24 16:15:37 -0700

    added logic and api url to recieve data in console now need to make logic to display on screen not yet completed

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const apiKey = "94591d084c8757a9ff2a1fbb69e284b4"
-    const weatherDetails = ['.cityTemp','.cityWind','.cityHumidity','.cityName'];
-    console.log(weatherDetails)
 
-    // Local storage clears after page is reloaded
-    localStorage.clear();
+    // const weatherDetails = ['.cityTemp','.cityWind','.cityHumidity','.cityName'];
+    // console.log(weatherDetails)
+
+    // // Local storage clears after page is reloaded
+    // localStorage.clear();
 
     // Fetch Weather Data/Event Listner
-$("#searchBtn").on("submit",function(event){
-    event.preventDefault();
-    const city = $("#cityInput").val()
+    $("#searchBtn").on("click", function (event) {
+        event.preventDefault();
+        const city = $("#cityInput").val()
+        const apiKey = "94591d084c8757a9ff2a1fbb69e284b4";
+        const URL1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`
 
- 
- fetch("")
-    .then
-    return response.json();
+        fetch(URL1 + apiKey)
+            .then(function (res) {
+                return res.json();
+            }).then(function (data) {
+                console.log(data.name);
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
+                const URL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=`
+                fetch(URL2 + apiKey)
+                    .then(function (res) {
+                        return res.json();
+                    }).then(function (data) {
+                        console.log(data);
+                        
 
-/* then functon*/
- 
-
-})
+                    })
+            })
 
 
+        /* then functon*/
+
+
+    })
 
 });
+
+
