e8bdc8a9896ab1568d838ca3fe0dd598697cff6c
kama casta <kamacasta983@gmail.com>
2021-09-01 22:53:29 -0700

    got the city input to show on the search history but need it to stay and accumilate when adding additional cities

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,33 @@
 $(document).ready(function () {
 
-
-    // const weatherDetails = ['.cityTemp','.cityWind','.cityHumidity','.cityName'];
-    // console.log(weatherDetails)
-
-    // // Local storage clears after page is reloaded
-    // localStorage.clear();
-
     // Fetch Weather Data/Event Listner
-    $("#searchBtn").on("click", function (event) {
-        event.preventDefault();
+    $("#searchBtn").on("click", function (event) { 
+        event.preventDefault(); 
+
+        var weatherHistory = []
+        // constants for city input, api key and both urls (one for current and other for weekly forecast)
         const city = $("#cityInput").val()
         const apiKey = "94591d084c8757a9ff2a1fbb69e284b4";
         const URL1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`
+        
 
-        var weatherHistory = JSON.parse(localStorage.getItem('weatherHistory'))  ?? [];
-        weatherHistory.push(city.value);
+        // variable created to show passed locations searched in local storage
+        // var weatherHistory = JSON.parse(localStorage.getItem('weatherHistory')) ?? [];
+        // weatherHistory.push(city.value);
+        // localStorage.setItem('weatherHistory', JSON.stringify('weatherHistory'));
+        weatherHistory.push(city);
         localStorage.setItem('weatherHistory', JSON.stringify(weatherHistory));
+        
+        // variable created to display search history in box under search bar
+        var list = document.getElementById('weatherHistory');
+        list.innerHTML = '';
+        for (var i = 0; i < weatherHistory.length; i++) {
+            var p = document.createElement('p');
+            p.appendChild(document.createTextNode(weatherHistory[i]));
+            list.appendChild(p);
+        }
 
+    
         fetch(URL1 + apiKey)
             .then(function (res) {
                 return res.json();
@@ -32,7 +42,9 @@ $(document).ready(function () {
                     }).then(function (data) {
                         console.log(data);
                     })
-            })
+            }) 
+            
+            weatherHistory = JSON.parse(localStorage.getItem('weatherHistory'));
 
 
         /* then functon*/
