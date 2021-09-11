0138312384dcafe3686474b4dcba144244175528
kama casta <kamacasta983@gmail.com>
2021-09-11 16:16:42 -0700

    added logic to fetch put the weatherHistory as a global variable and added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,57 +1,67 @@
 $(document).ready(function () {
+  var weatherHistory = [];
 
-    // Fetch Weather Data/Event Listner
-    $("#searchBtn").on("click", function (event) { 
-        event.preventDefault(); 
+  // Fetch Weather Data/Event Listner
+  $("#searchBtn").on("click", function (event) {
+    event.preventDefault();
 
-        var weatherHistory = []
-        // constants for city input, api key and both urls (one for current and other for weekly forecast)
-        const city = $("#cityInput").val()
-        const apiKey = "94591d084c8757a9ff2a1fbb69e284b4";
-        const URL1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`
-        
+    // constants for city input, api key and both urls (one for current and other for weekly forecast)
+    const city = $("#cityInput").val();
+    const apiKey = "94591d084c8757a9ff2a1fbb69e284b4";
+    const URL1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`;
 
-        // variable created to show passed locations searched in local storage
-        // var weatherHistory = JSON.parse(localStorage.getItem('weatherHistory')) ?? [];
-        // weatherHistory.push(city.value);
-        // localStorage.setItem('weatherHistory', JSON.stringify('weatherHistory'));
-        weatherHistory.push(city);
-        localStorage.setItem('weatherHistory', JSON.stringify(weatherHistory));
-        
-        // variable created to display search history in box under search bar
-        var list = document.getElementById('weatherHistory');
-        list.innerHTML = '';
-        for (var i = 0; i < weatherHistory.length; i++) {
-            var p = document.createElement('p');
-            p.appendChild(document.createTextNode(weatherHistory[i]));
-            list.appendChild(p);
-        }
+    // variable created to show passed locations searched in local storage
+    weatherHistory.push(city);
+    localStorage.setItem("weatherHistory", JSON.stringify(weatherHistory));
 
-    
-        fetch(URL1 + apiKey)
-            .then(function (res) {
-                return res.json();
-            }).then(function (data) {
-                console.log(data.name);
-                const lat = data.coord.lat;
-                const lon = data.coord.lon;
-                const URL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=`
-                fetch(URL2 + apiKey)
-                    .then(function (res) {
-                        return res.json();
-                    }).then(function (data) {
-                        console.log(data);
-                    })
-            }) 
-            
-            weatherHistory = JSON.parse(localStorage.getItem('weatherHistory'));
+    // variable created to display search history in box under search bar
+    var list = document.getElementById("weatherHistory");
+    list.innerHTML = "";
+    for (var i = 0; i < weatherHistory.length; i++) {
+      var p = document.createElement("p");
+      p.appendChild(document.createTextNode(weatherHistory[i]));
+      list.appendChild(p);
+    }
 
+    //
+    fetch(URL1 + apiKey)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        console.log(data.name);
+        $("#cityName").text(data.name);
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+        const URL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=`;
+        fetch(URL2 + apiKey)
+          .then(function (res) {
+            return res.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            $("#temp").text(data.current.temp);
+            $("#hum").text(data.current.humidity);
+            $("#wind").text(data.current.wind_speed);
+            $("#uvi").text(data.current.uvi);
 
-        /* then functon*/
+            for (let i = 0; i < 5; i++) {
+              console.log(data.daily[i]);
+              var card = `
+                            <div class="card">
+                             <div class="card-content">
+                              <div class="content">
+                              <span>Temperature: &#176${data.daily[i].temp.day}</span>
+                               </div>
+                              </div>
+                            </div>
+                            `;
+                $("#forecastContainer").append(card)
+            }
+          });
+      });
 
-
-    })
-
+    /* then functon*/
+    weatherHistory = JSON.parse(localStorage.getItem("weatherHistory"));
+  });
 });
-
-
